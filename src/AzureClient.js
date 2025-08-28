ebfe1ba388d6a9fba9885ee64924d28746ec84fc
Stanislav Atanasov <stanchyyy@gmail.com>
2025-08-28 03:58:31 +0300

    removed secret from code

--- src/AzureClient.js
+++ src/AzureClient.js
@@ -5,7 +5,7 @@ import createClient from '@azure-rest/ai-vision-image-analysis';
 function AzureClient(){
 
   const endpoint = process.env.REACT_APP_VISION_ENDPOINT || 'https://exercise-add-image-analysis-capabilities-to-your-react-app.cognitiveservices.azure.com/';
-  const key = process.env.REACT_APP_VISION_KEY || '3yElsz9yw555qJOG0jlYBhaqlah00KEqveooYZKTWJj3LQUVhZ0bJQQJ99BHACYeBjFXJ3w3AAAFACOGGHPI';
+  const key = process.env.REACT_APP_VISION_KEY ;
   console.log('Using endpoint:', process.env.REACT_APP_VISION_ENDPOINT);
   const credential = new AzureKeyCredential(key);
   const client = createClient(endpoint, credential);
